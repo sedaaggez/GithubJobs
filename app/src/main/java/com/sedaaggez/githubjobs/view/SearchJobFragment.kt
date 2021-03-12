aa7dee948fb4ea88f9307cc42fbab429144e12a1
sedaaggez <sedaaggez.koctas@gmail.com>
2021-03-12 20:05:16 +0300

    set navigation

---
+++ app/src/main/java/com/sedaaggez/githubjobs/view/SearchJobFragment.kt
@@ -0,0 +1,23 @@
+package com.sedaaggez.githubjobs.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sedaaggez.githubjobs.R
+
+class SearchJobFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_job, container, false)
+    }
+
+}
