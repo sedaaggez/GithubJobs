8377ac09f36c3fa318ef5e4912c7a31707f42965
sedaaggez <sedaaggez.koctas@gmail.com>
2021-04-03 22:15:20 +0300

    create BaseViewModel getData(job description) JobsFragment add description argument

--- app/src/main/java/com/sedaaggez/githubjobs/view/SearchJobFragment.kt
+++ app/src/main/java/com/sedaaggez/githubjobs/view/SearchJobFragment.kt
@@ -5,12 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.sedaaggez.githubjobs.R
+import kotlinx.android.synthetic.main.fragment_search_job.*
 
 class SearchJobFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        buttonSubmit.setOnClickListener(View.OnClickListener {
+            if (editTextSearchJob.text.isNullOrEmpty()) {
+                Toast.makeText(context, "Please do not leave blank!", Toast.LENGTH_SHORT).show()
+            } else {
+                val description = editTextSearchJob.text.toString()
+                val action = SearchJobFragmentDirections.actionSearchJobFragmentToJobsFragment(description)
+                Navigation.findNavController(it).navigate(action)
+            }
+        })
 
     }
 
