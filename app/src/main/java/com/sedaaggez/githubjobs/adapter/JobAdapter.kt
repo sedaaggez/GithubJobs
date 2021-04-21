a0bb0569a902b0400b1ddb2c32248f3a81579021
sedaaggez <seda-aggez@hotmail.com>
2021-04-21 17:39:00 +0300

    item (recyclerview) data binding

--- app/src/main/java/com/sedaaggez/githubjobs/adapter/JobAdapter.kt
+++ app/src/main/java/com/sedaaggez/githubjobs/adapter/JobAdapter.kt
@@ -1,42 +1,27 @@
 package com.sedaaggez.githubjobs.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.sedaaggez.githubjobs.R
+import com.sedaaggez.githubjobs.databinding.ItemJobBinding
 import com.sedaaggez.githubjobs.model.Job
-import com.sedaaggez.githubjobs.util.downloadFromUrl
-import com.sedaaggez.githubjobs.util.placeholderProgressBar
-import com.sedaaggez.githubjobs.view.JobsFragmentDirections
-import kotlinx.android.synthetic.main.item_job.view.*
 
 class JobAdapter(val jobList: ArrayList<Job>) : RecyclerView.Adapter<JobAdapter.JobViewHolder>() {
 
-    class JobViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+    class JobViewHolder(var view: ItemJobBinding) : RecyclerView.ViewHolder(view.root) {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_job, parent, false)
+        val view = DataBindingUtil.inflate<ItemJobBinding>(inflater, R.layout.item_job, parent, false)
         return JobViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: JobViewHolder, position: Int) {
-        holder.view.textViewTitle.text = jobList[position].title
-        holder.view.textViewCompany.text = jobList[position].company
-        holder.view.textViewLocation.text = jobList[position].location
-        holder.view.textViewType.text = jobList[position].type
-        holder.view.imageViewCompanyLogo.downloadFromUrl(
-            jobList[position].companyLogo,
-            placeholderProgressBar(holder.view.context)
-        )
-        holder.view.setOnClickListener {
-            val action =
-                JobsFragmentDirections.actionJobsFragmentToJobDetailFragment(jobList[position].uuid)
-            Navigation.findNavController(it).navigate(action)
-        }
+        holder.view.job = jobList[position]
+        // TODO: Listener bindings
     }
 
     override fun getItemCount(): Int {
